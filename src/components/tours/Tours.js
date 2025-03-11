b018f64cb544907d38d50626f8b5e0c259c0c426
Your Name <you@example.com>
2025-03-11 15:30:00 +0530

    first commit

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,36 +1,36 @@
 import React from "react";
 import TourCategories from "./TourCategories/TourCategories";
-// import { getTrekCategories } from "@/services";
-// import Loader from "@/common/loader";
-// import { useTour } from "@/context/tourcontext/TourContaxt";
+import { getTrekCategories } from "@/services";
+import Loader from "@/common/loader";
+import { useTour } from "@/context/tourcontext/TourContaxt";
 
 export const Tours = ({ allTrekCategories }) => {
-  // const [allTrekCategories, setAllTrekCategories] = useState([]);
-  // const [loading, setLoading] = useState(true);
+  const [allTrekCategories, setAllTrekCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
   
 
-  // const fetchAllTrekCategories = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const res = await getTrekCategories();
-  //     // console.log(res.data.data.items, "trek categories");
-  //     setAllTrekCategories(res.data.data.items);
-  //   } catch (error) {
-  //     console.log("error in feching trek categories", error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+  const fetchAllTrekCategories = async () => {
+    setLoading(true);
+    try {
+      const res = await getTrekCategories();
+      // console.log(res.data.data.items, "trek categories");
+      setAllTrekCategories(res.data.data.items);
+    } catch (error) {
+      console.log("error in feching trek categories", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchAllTrekCategories();
-  // }, []);
+  useEffect(() => {
+    fetchAllTrekCategories();
+  }, []);
 
 
 
-  // if (loading) {
-  //   return <Loader />;
-  // }
+  if (loading) {
+    return <Loader />;
+  }
 
   return <TourCategories allTrekCategories={allTrekCategories} />;
 };
