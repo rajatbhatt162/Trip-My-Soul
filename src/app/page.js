1a55f2e1ffb8ae087d25f94aaf1314d5b9893de2
Your Name <you@example.com>
2025-03-11 15:25:29 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,83 +1,3 @@
-// import { Box } from "@mui/material";
-// import TravelBanner from "../components/travelbannermainpage";
-// import IdealTravelCampanion from "../components/idealtravelcampanion";
-// import Certifications from "../components/certifications";
-// import { Tours } from "../components/tours/Tours";
-// import HomePageFaQ from "@/components/homepagefaq";
-// import ThankYouModal from "@/components/thankyoumodel";
-// import { getTrekCategories } from "@/services";
-// import { transformTrekCategories } from "@/utils/transformTrekCategories";
-
-// const getData = async () => {
-//   let initialPageData = {
-//     treks: [],
-//     trekLimits: [],
-//     regions: [],
-//     allTrekCategories: [],
-//   };
-
-//   try {
-//     let res = {};
-//     try {
-//       res = await getTrekCategories();
-//     } catch (e) {
-//       throw e;
-//     }
-
-//     const trekCategories = transformTrekCategories(res.data.data.items);
-//     const filteredCategories = trekCategories.filter((category) =>
-//       [
-//         "uttarakhand-110",
-//         "kashmir-122",
-//         "himachal-pardesh-112",
-//         "trending-treks-on-tms-103",
-//       ].includes(category.id)
-//     );
-//     const initialLimits = {};
-//     filteredCategories.forEach((category) => {
-//       initialLimits[category.id] = 6;
-//     });
-
-//     initialPageData.treks = trekCategories[0].treks;
-//     initialPageData.trekLimits = initialLimits;
-//     initialPageData.regions = filteredCategories;
-//     initialPageData.allTrekCategories = res.data.data.items;
-//     return initialPageData;
-//   } catch (e) {
-//     console.log("Error in initial rendering", e);
-//     return initialPageData;
-//   }
-// };
-
-// export default async function Home() {
-//   let initialData = {};
-//   try {
-//     initialData = await getData();
-//   } catch (e) {
-//     throw e;
-//   }
-
-//   console.log("initial data in tms in home page", initialData);
-//   return (
-//     <Box>
-//       <TravelBanner />
-//       <Tours allTrekCategories={initialData.allTrekCategories} />
-//       <IdealTravelCampanion />
-//       <HomePageFaQ />
-//       <Certifications />
-
-//       <ThankYouModal />
-//     </Box>
-//   );
-// }
-
-// export async function generateMetadata({ params }) {
-//   return {
-//     title: "Trip My Soul | Unforgettable Travel, Trekking & Adventure Tours",
-//     description:
-//       "Tripmysoul.in-one of the top India trekking companies in India .Experience adventure trekking & camping with expert guides & trusted experience |Book now",
-//   };
-// }
 import { Box } from "@mui/material";
 import TravelBanner from "../components/travelbannermainpage";
 import IdealTravelCampanion from "../components/idealtravelcampanion";
@@ -97,15 +17,17 @@ const getData = async () => {
   };
 
   try {
-    let res = {};
-    try {
-      res = await getTrekCategories();
-    } catch (e) {
-      throw e;
+    console.log('Fetching trek categories from:', process.env.NEXT_PUBLIC_BASE_URL);
+    const res = await getTrekCategories();
+    
+    if (!res?.data?.data?.items) {
+      console.error('Invalid response structure:', res);
+      return initialPageData; // Return empty data instead of throwing
     }
 
     const trekCategories = transformTrekCategories(res.data.data.items);
-    
+    console.log('Transformed trek categories:', trekCategories.length);
+
     // Define the same category order as in TourCategories.js
     const categoryOrder = [
       "Spirituals-Journey-101",
@@ -131,34 +53,46 @@ const getData = async () => {
       initialLimits[category.id] = 6;
     });
 
-    initialPageData.treks = orderedCategories[0]?.treks || [];
-    initialPageData.trekLimits = initialLimits;
-    initialPageData.regions = orderedCategories;
-    initialPageData.allTrekCategories = res.data.data.items;
+    initialPageData = {
+      treks: orderedCategories[0]?.treks || [],
+      trekLimits: initialLimits,
+      regions: orderedCategories,
+      allTrekCategories: res.data.data.items,
+    };
+
     return initialPageData;
   } catch (e) {
-    console.log("Error in initial rendering", e);
-    return initialPageData;
+    console.error("Error in getData:", e);
+    console.error("Error details:", {
+      message: e.message,
+      response: e.response?.data,
+      status: e.response?.status,
+    });
+    return initialPageData; // Return empty data instead of throwing
   }
 };
 
 export default async function Home() {
-  let initialData = {};
+  let initialData = {
+    treks: [],
+    trekLimits: [],
+    regions: [],
+    allTrekCategories: [],
+  };
+  
   try {
     initialData = await getData();
   } catch (e) {
-    throw e;
+    console.error("Error in Home component:", e);
   }
 
-  console.log("initial data in tms in home page", initialData);
   return (
     <Box>
       <TravelBanner />
-      <Tours allTrekCategories={initialData.allTrekCategories} />
+      <Tours allTrekCategories={initialData.allTrekCategories || []} />
       <IdealTravelCampanion />
       <HomePageFaQ />
       <Certifications />
-
       <ThankYouModal />
     </Box>
   );
